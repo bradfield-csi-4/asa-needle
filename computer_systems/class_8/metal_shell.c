62ff90a9dca82823551ab9bffb096e00f4e4ddf7
Asa Needle <asa@asaneedle.com>
2022-09-01 13:24:55 -0400

    meets reqs 1-4

--- computer_systems/class_8/metal_shell.c
+++ computer_systems/class_8/metal_shell.c
@@ -4,12 +4,19 @@
 #include <sys/syscall.h>
 #include <pwd.h> 
 #include <sched.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
 
-int handle_input(char input[100]);
+int handle_input(char *token, char input[100]);
+int handle_process(char input[100]);
+void sighandler(int dummy);
 
 #ifndef MAX_BUF
 #define MAX_BUF 200
 #endif
+
+// add a tokenize function
 
 int main (){
     int c, i;
@@ -19,8 +26,12 @@ int main (){
 	while ((c = getchar()) != EOF) {
         if (c == '\n') {
             input[i] = '\0';
-            int result = handle_input(input);
+            signal(SIGINT, sighandler);
+            int result = handle_process(input);
+            signal(SIGINT, SIG_DFL);
+
             if (result < 0){
+                printf("ending loop due to ctrl-c\n");
                 break;
             }
             input[0] = '\0';
@@ -31,14 +42,46 @@ int main (){
             i++;
         }
     }
+    printf("c was %d\n", c == EOF);
     printf("\n😈😈😈😈 ROCK ON! 😈😈😈😈\n");
 }
 
-int handle_input(char input[100]){
+void sighandler(int dummy){
+    printf("\n");
+}
+
+int handle_process(char input[100]){
+    int status = 0;
+    pid_t wpid;
+
+    int fd[2];
+
     char *token;
     token = strtok(input, " ");
     int cmp_result;
 
+    cmp_result = strcmp(input, "exit");
+    if (cmp_result == 0){ 
+        return -1;
+    }
+
+    struct sigaction *myaction;
+
+    if (fork() == 0){
+        // CHILD
+        int result = handle_input(token, input);
+        exit(0);
+    } else {
+        // PARENT
+        while ((wpid = wait(&status)) > 0);
+        return 1;
+    }
+}
+
+int handle_input(char *token, char input[100]){
+
+    int cmp_result;
+    
     cmp_result = strcmp(token, "whoami");
     if (cmp_result == 0){ 
         token = strtok(NULL, " ");
@@ -78,10 +121,25 @@ int handle_input(char input[100]){
         return 1;
     }
 
-    cmp_result = strcmp(input, "exit");
+    cmp_result = strcmp(input, "sleep");
     if (cmp_result == 0){ 
-        return -1;
+        token = strtok(NULL, " ");
+        if (token == NULL) {
+            printf("usage: sleep seconds");
+            return 1;
+        } else {
+            char *seconds = token;
+            token = strtok(NULL, " ");
+            if (token != NULL) {
+                printf("usage: sleep seconds");
+                return 1;
+            } else {
+                sleep(atoi(seconds));
+                return 1;
+            }
+        } 
     }
+
     printf("😭 %s: command not found\n", input);
     return 1;
 }
